4628f41dd6609da90379f04d43604e20e4b6edd1
Jared <jared@Zipcoders-MacBook-Pro-4.local>
2024-08-16 13:29:26 -0400

    done

--- src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
+++ src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class BakerController {
-    @Autowired
     private BakerService service;
 
     public BakerController(BakerService service) {
